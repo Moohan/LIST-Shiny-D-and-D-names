0237ec69a13ac839d28be6b141aab242e76afdaf
James Hayes <james.hayes2@phs.scot>
2025-09-16 14:49:47 +0000

    End of day

--- app.R
+++ app.R
@@ -1,104 +1,53 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(shinydashboard)
 library(readr)
-library(ggplot2)
 library(dplyr)
-library(glue)
-library(leaflet)
-library(lubridate)
-
+library(stringr)
+library(stringdist)
 
 # ////////////////////////////////////////////////////////////////////////////
 # Setup
 # ////////////////////////////////////////////////////////////////////////////
-saturday_bikes <- read_csv("data/saturday_bikes.csv")
+names <- read_csv("https://raw.githubusercontent.com/janelleshane/DnD-characters/refs/heads/master/DnD_characters_May2018.txt", show_col_types = FALSE) |> 
+  rename(name = 1, race = 2, class = 3) 
 
-
+names <- names |> 
+  mutate(
+    race = sample(sample(race, 3), nrow(names), TRUE),
+    class = sample(sample(class, 3), nrow(names), TRUE)
+  )
 # ////////////////////////////////////////////////////////////////////////////
 # UI
 # ////////////////////////////////////////////////////////////////////////////
-ui <- dashboardPage(
-  skin = "red",
-  dashboardHeader(title = "Capitol Bikeshare Availability - May 14, 2022"),
-  dashboardSidebar(disable = TRUE),
-  dashboardBody(
-    box(title = "Station Map",
-        leafletOutput("map"),
-        width = 12
-    ),
-    box("Click a station to populate",
-        width = 12,
-        plotOutput("plot")
-    )
-  )
+ui <- fluidPage(
+  titlePanel(title = "D&D Names 🐲"),
+        textInput("name-input", "Your Name"),
+        radioButtons("class-buttons", "Class", choices = unique(names$class)),
+        radioButtons("race-buttons", "Race", choices = unique(names$race)),
+        actionButton("show_text", "Go go magic name generator!"),
+        textOutput("name-output")
 )
 
 # ////////////////////////////////////////////////////////////////////////////
 # Server
 # ////////////////////////////////////////////////////////////////////////////
 server <- function(input, output, session) {
-  
-  # Draw station map ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  output$map <- renderLeaflet({
-    saturday_bikes %>%
-      leaflet() %>%
-      addProviderTiles(providers$CartoDB.Positron) %>%
-      setView(
-        lng = median(saturday_bikes$lon),
-        lat = median(saturday_bikes$lat),
-        zoom = 13
-      ) %>%
-      addAwesomeMarkers(
-        lng = ~lon,
-        lat = ~lat,
-        layerId = ~station_id,
-        icon = awesomeIcons(
-          "bicycle",
-          library = "fa",
-          iconColor = "white",
-          markerColor = "red"
-        ),
-        
-        label = ~paste0(name)
-      )
+  text_reactive <- eventReactive(input$show_text, {
+    dd_name <- names |> 
+      filter(
+        class == input$class-buttons,
+        race == input$race-buttons,
+        stringr::str_detect(name, input$name-input, negate = TRUE)
+      ) |> 
+      mutate(dist = stringdist::stringdist(input$name-input, name))  |>
+      slice_min(dist, n = 1, with_ties = FALSE) |> 
+      pull(name)
+    
+    return(dd_name)
   })
   
-  
-  # Get availability ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  availability <- reactive({
-    
-    req(input$map_marker_click)
-    
-    saturday_bikes %>%
-      filter(station_id == input$map_marker_click$id)
+  output$name-output <- renderText({
+    text_reactive()
   })
-  
-  # Create plot of availability~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  output$plot <- renderPlot({
-    
-    station_name <- availability() %>%
-      distinct(name) %>%
-      pull()
-    
-    availability() %>%
-      ggplot(aes(x = time, y = num_bikes_available)) +
-      geom_line() +
-      ggtitle(glue::glue("Bikes Available at {station_name}")) +
-      xlab("Time") +
-      ylab("Number of Bikes") +
-      scale_x_datetime(labels = function(x) format(x - 18000, "%H:%M")) +
-      scale_y_continuous(labels = round)
-  })
-  
 }
 
 # ////////////////////////////////////////////////////////////////////////////
